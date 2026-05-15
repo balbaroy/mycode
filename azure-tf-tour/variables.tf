b2f502cd77de447b5352eb214613e6ab9c20440a
balbaroy <mbg100@outlook.com>
2026-05-15 19:46:14 +0000

    terraform and Azure

---
+++ azure-tf-tour/variables.tf
@@ -0,0 +1,18 @@
+# this is now an "input" value 
+variable "build_location" {
+  type = string
+  default = "westus2"
+}
+
+# this is now an "input" value 
+variable "rg_name" {
+  type = string
+  default = "myAzureRG"
+
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "The name of the Azure Virtual Network."
+  default     = "my-vnet-name"
+}
